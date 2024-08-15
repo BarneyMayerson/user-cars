7c01dcaad88d83bd77baa872ea931d012965f6ee
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-08-15 11:01:10 +0300

    Basics && First Tests

---
+++ tests/Feature/CarListTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Livewire\CarList;
+use App\Models\Brand;
+use App\Models\Car;
+use Livewire\Livewire;
+
+it('render the car list component', function () {
+    Livewire::test(CarList::class)->assertOk();
+});
+
+it('shows brand, model and year', function () {
+    Car::factory()
+        ->for(Brand::factory()->create(['name' => 'Mazda']))
+        ->create([
+            'model' => 'MX-5',
+            'year' => 2005,
+        ]);
+
+    Car::factory()
+        ->for(Brand::factory()->create(['name' => 'Nissan']))
+        ->create([
+            'model' => 'Tiida',
+            'year' => 2018,
+        ]);
+
+    Car::factory()
+        ->for(Brand::factory()->create(['name' => 'Ford']))
+        ->create([
+            'model' => 'Mustang',
+            'year' => 2012,
+        ]);
+
+    Livewire::test(CarList::class)
+        ->assertOk()
+        ->assertSeeText([
+            'Mazda', 'MX-5', '2005',
+            'Nissan', 'Tiida', '2018',
+            'Ford', 'Mustang', '2012',
+        ]);
+});
--- tests/Unit/Models/BrandTest.php
+++ tests/Unit/Models/BrandTest.php
@@ -1,5 +1,5 @@
 <?php
 
-test('that true is true', function () {
+test('models/brand', function () {
     expect(true)->toBeTrue();
 });
